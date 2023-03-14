ed7f7c88d91af8544d3717626e723582ea89fb02
Rodrigo <rodrigo.del.castillo.-nd@disney.com>
2023-03-14 12:28:42 -0500

    fix typo

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,7 @@
-resource "aws_ecr_repository" "pichincha_challenge_ecr" {
-  name = "pichincha-challenge"
+# resource "aws_ecr_repository" "pichincha_challenge_ecr" {
+#   name = "pichincha-challenge"
   
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
+#   image_scanning_configuration {
+#     scan_on_push = true
+#   }
+# }
--- scripts/instance-init.sh
+++ scripts/instance-init.sh
@@ -12,8 +12,8 @@ sudo apt install docker.io -y
 
 sudo mkdir ~/.aws && touch ~/.aws/credentials
 echo "[dev-practice]
-aws_access_key_id = ${{ secrets.AWS_ACCESS_KEY_ID }}
-aws_secret_access_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 region = us-east-1" >> ~/.aws/credentials 
 
 export AWS_PROFILE=dev-practice
@@ -22,4 +22,4 @@ aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS
 
 sudo docker pull 446427957983.dkr.ecr.us-east-1.amazonaws.com/pichincha-challenge:latest
 
-sudo docker run -p 80:8080 446427957983.dkr.ecr.us-east-1.amazonaws.com/pichincha-challenge:latest
+sudo docker run -p 80:8080 446427957983.dkr.ecr.us-east-1.amazonaws.com/pichincha-challenge:$IMAGE_TAG
